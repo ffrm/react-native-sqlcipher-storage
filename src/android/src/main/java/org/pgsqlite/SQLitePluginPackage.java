9c3f16f95ecfe7eab19d0de883e06df1d0f5bf36
Fernando Rama <fernandorama.dev@gmail.com>
2019-05-02 13:05:09 -0300

    Update android source to support new React Native versions

--- src/android/src/main/java/org/pgsqlite/SQLitePluginPackage.java
+++ src/android/src/main/java/org/pgsqlite/SQLitePluginPackage.java
@@ -7,16 +7,13 @@ package org.pgsqlite;
 
 import android.app.Activity;
 
+import java.util.*;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-import java.util.List;
 
 public class SQLitePluginPackage implements ReactPackage {
 
@@ -32,16 +29,12 @@ public class SQLitePluginPackage implements ReactPackage {
     }
 
     @Override
-    public List<NativeModule> createNativeModules(
-                                ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       List<NativeModule> modules = new ArrayList<>();
-
       modules.add(new SQLitePlugin(reactContext));
-
       return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
